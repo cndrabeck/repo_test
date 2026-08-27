d4ca76ee503f815ed5502bc43d6a0dc14bd90bd8
cndrabeck <cnd88@miami.edu>
2026-08-27 10:03:10 -0400

    Adding code and figure for lionfish biometric analysis

---
+++ scripts/03_content/my_first_plot.R
@@ -0,0 +1,12 @@
+library(EVR628tools)
+library(tidyverse)
+# Load data
+data(data_lionfish)
+
+# Create a simple plot
+p <- ggplot(data_lionfish,
+            aes(x = total_length_mm, y = total_weight_gr)) +
+  geom_point()
+
+p
+ggsave(plot = p, filename = "results/img/first_plot.png")
